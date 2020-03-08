76de068190293d4cd643222211e1470be630ea84
Daniel <01127163@pw.edu.pl>
2020-03-08 22:27:29 +0100

    simplified surname validation regex

--- src/main/kotlin/pl/komjago/ticketapp/util/validators/SurnameValidator.kt
+++ src/main/kotlin/pl/komjago/ticketapp/util/validators/SurnameValidator.kt
@@ -7,8 +7,7 @@ class SurnameValidator : ConstraintValidator<SurnameConstraint, String> {
     override fun initialize(surnameConstraint: SurnameConstraint) {}
 
     override fun isValid(surname: String, cxt: ConstraintValidatorContext?): Boolean {
-        val surnameRegex = Regex("\\p{Lu}\\p{Ll}{2,}")
-        val doubleSurnameRegex = Regex("\\p{Lu}\\p{Ll}{2,}-\\p{Lu}\\p{Ll}{2,}|\\p{Lu}\\p{Ll}{2,}")
-        return (if (surname.contains('-')) doubleSurnameRegex.matches(surname) else surnameRegex.matches(surname))
+        val surnameRegex = Regex("\\p{Lu}\\p{Ll}{2,}(-\\p{Lu}\\p{Ll}{2,})?")
+        return surnameRegex.matches(surname)
     }
 }
