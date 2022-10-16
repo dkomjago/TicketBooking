0a0185edda62ba3b94186325947d1804cc159249
Daniel K <komjago@outlook.com>
2022-10-16 17:59:46 +0200

    Fixed use case scripts to match new api

--- booking-app-use-case-example-auto.sh
+++ booking-app-use-case-example-auto.sh
@@ -8,4 +8,4 @@ printf "\n///\nSELECT SCREENING -> GET AVAILABLE SEATS\n///\n"
 screeningId=$(curl -sS -X GET "http://$appHost:$appPort/api/booking/screenings?from=2016-11-16%2006%3A43&to=2030-11-16%2006%3A43" -H  "accept: */*" | jq ".screenings[0].id")
 curl -sS -X GET "http://$appHost:$appPort/api/booking/screening/$screeningId" -H  "accept: */*" | jq .
 printf "\n///\nBOOK SEAT -> GET PRICE AND EXPIRATION TIME\n///\n"
-curl -sS -X POST "http://$appHost:$appPort/api/booking/book" -H  "accept: */*" -H  "Content-Type: application/json" -d "{  \"selectedSeats\": [    {      \"ticketTypeId\": 0,      \"seatId\": 1    }  ],  \"screeningId\": $screeningId,  \"name\": \"String\",  \"surname\": \"String\"}" | jq .
+curl -sS -X POST "http://$appHost:$appPort/api/booking/book" -H  "accept: */*" -H  "Content-Type: application/json" -d "{  \"selectedSeats\": [    {      \"ticketTypeId\": 0,      \"seatId\": 1    }  ],  \"screeningId\": $screeningId,  \"name\": \"String\"}" | jq .
--- booking-app-use-case-example-manual.sh
+++ booking-app-use-case-example-manual.sh
@@ -3,7 +3,7 @@ appHost=${1:-localhost}
 appPort=${2:-8080}
 
 printf "\n///\nSTART\n///\n"
-curl -sS -X GET "http://$appHost:$appPort/api/booking/screenings/api/booking/screenings?from=2016-11-16%2006%3A43&to=2020-11-16%2006%3A43" -H  "accept: */*"
+curl -sS -X GET "http://$appHost:$appPort/api/booking/screenings?from=2016-11-16%2006%3A43&to=2030-11-16%2006%3A43" -H  "accept: */*"
 printf "\n!!!Please enter screening id!!!\n"
 read -r screeningId
 curl -sS -X GET "http://$appHost:$appPort/api/booking/screening/$screeningId" -H  "accept: */*"
@@ -13,7 +13,5 @@ printf "\n!!!Please enter ticket type id!!!\n"
 read -r ticketTypeId
 printf "\n!!!Please enter name!!!\n"
 read -r customerName
-printf "\n!!!Please enter surname!!!\n"
-read -r customerSurname
-curl -sS -X POST "http://$appHost:$appPort/api/booking/book" -H  "accept: */*" -H  "Content-Type: application/json" -d "{  \"selectedSeats\": [    {      \"ticketTypeId\": $ticketTypeId,      \"seatId\": $seatId    }  ],  \"screeningId\": $screeningId,  \"name\": \"$customerName\",  \"surname\": \"$customerSurname\"}"
+curl -sS -X POST "http://$appHost:$appPort/api/booking/book" -H  "accept: */*" -H  "Content-Type: application/json" -d "{  \"selectedSeats\": [    {      \"ticketTypeId\": $ticketTypeId,      \"seatId\": $seatId    }  ],  \"screeningId\": $screeningId,  \"name\": \"$customerName\"}"
 printf "\n///\nEND\n///\n"
